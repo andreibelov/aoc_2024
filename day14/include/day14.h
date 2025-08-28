75a1366f0853cf33e8d94966ecf8577225aa4e6e
Andrei Belov <belovsmail@gmail.com>
2025-08-28 16:52:22 +0100

    day14: init part1

--- day14/include/day14.h
+++ day14/include/day14.h
@@ -13,18 +13,48 @@
 #ifndef DAY14_H
 #define DAY14_H
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <glib.h>
+#include <sys/queue.h>
+//#include <linux/list.h>
+#include <utlist.h>
+#include <stdlib.h>
 #include "aoc_2024_defs.h"
 
 #ifndef INPUT_FILE_NAME
 # define INPUT_FILE_NAME "./resources/input"
 #endif
 
+//#define AREA_WIDTH 101
+//#define AREA_HEIGHT 103
+
+#define AREA_WIDTH 11
+#define AREA_HEIGHT 7
+
+typedef struct Robot
+{
+	Point p;
+	Point v;
+	LIST_ENTRY(Robot) link; /* intrusive linkage */
+}	Robot;
+
+LIST_HEAD(RobotList, Robot);
+
+typedef struct s_item
+{
+	char			*str;
+	struct s_item	*next;
+}	StringListItem;
+
 typedef struct s_info
 {
+	char				area[AREA_WIDTH * AREA_HEIGHT];
+	int					waitTime;
+	struct RobotList	robots;
 }	t_info;
 
 int part1(t_info *game);
--- day14/src/main.c
+++ day14/src/main.c
@@ -15,20 +15,76 @@
 
 int main(void)
 {
-	t_info	info = {};
-	prepare(&info);
+	t_info	info = { .waitTime = 100 };
+	t_info	*const game = &info;
+	prepare(game);
 
-	printf("part1: %d\n=====================================\n", part1(&info));
-	printf("part2: %ld\n", part2(&info));
+	printf("part1: %d\n=====================================\n", part1(game));
+	printf("part2: %ld\n", part2(game));
+
+	Robot *rob = LIST_FIRST(&game->robots), *temp;
+	while (rob != NULL) {
+		temp = LIST_NEXT(rob, link);
+		LIST_REMOVE(rob, link);  /* unlink from list */
+		free(rob);               /* free memory */
+		rob = temp;
+	}
 	return (EX_OK);
 }
 
 int	part1(t_info *game)
 {
-	int result = 0;
+	int			i;
+	const int	waitTime = game->waitTime;
+	char *const	area = game->area;
+	int			result = 0;
+	Robot		*r, robot;
 
-	(void)game;
-	; // implement part1 here
+	i = -1;
+	while (++i < waitTime) {
+		LIST_FOREACH(r, &game->robots, link) {
+			robot = *r;
+
+			robot.p.x = (robot.p.x + robot.v.x + AREA_WIDTH) % AREA_WIDTH;
+			robot.p.y = (robot.p.y + robot.v.y + AREA_HEIGHT) % AREA_HEIGHT;
+
+			*r = robot;
+		}
+	}
+
+	memset(area, 0, AREA_WIDTH * AREA_HEIGHT);
+
+	LIST_FOREACH(r, &game->robots, link) {
+		robot = *r;
+		area[robot.p.y * AREA_WIDTH + robot.p.x]++;
+	}
+
+	Point it = { .y = -1};
+	while (++it.y < AREA_HEIGHT) {
+		it.x = -1;
+		while (++it.x < AREA_WIDTH) {
+			char c = area[it.y * AREA_WIDTH + it.x];
+			fprintf(stdout, "%c", c == 0 ? '.' : c + '0');
+		}
+		fprintf(stdout, "\n");
+	}
+	fprintf(stdout, "\n");
+	it.y = -1;
+	while (++it.y < AREA_HEIGHT) {
+		it.x = -1;
+		while (++it.x < AREA_WIDTH) {
+			char c = area[it.y * AREA_WIDTH + it.x];
+			if (it.x == (AREA_WIDTH / 2) || it.y == (AREA_HEIGHT / 2)) {
+				c = ' ';
+			} else {
+				c = (char)(c == 0 ? '.' : c + '0');
+			}
+			fprintf(stdout, "%c", c);
+		}
+		fprintf(stdout, "\n");
+	}
+
+	fflush(stdout);
 	return (result);
 }
 
@@ -43,21 +99,57 @@ long	part2(t_info *game)
 
 void prepare(t_info *game)
 {
-	char	*buf = NULL;
-	size_t	size;
-	ssize_t	len;
-	FILE	*f;
+	char			*buf = NULL;
+	size_t			size;
+	ssize_t			len;
+	FILE			*f;
+	StringListItem	*list = NULL, *it, *tmp;
 
+	game->robots = (struct RobotList)LIST_HEAD_INITIALIZER(robots);
 	f = fopen(INPUT_FILE_NAME, "r");
 
 	size = 0;
 	len = getline(&buf, &size, f);
 	while (len != -1)
 	{
-		(void)game;
-		; // write initialization code here
+		it = malloc(sizeof(StringListItem));
+		it->str = strndup(buf, strchrnul(buf, '\n') - buf);
+		LL_APPEND(list, it);
 		len = getline(&buf, &size, f);
 	}
 	free(buf);
 	fclose(f);
+
+	LL_FOREACH(list, it)
+	{
+		char *ptr;
+		char *tok = strtok_r(it->str, " ", &ptr);
+
+		Robot robot;
+
+		robot.p.x = atoi(strtok(strchrnul(tok, '=') + 1, ","));
+		robot.p.y = atoi(strtok(NULL, ","));
+
+		robot.v.x = atoi(strtok(strchrnul(ptr, '=') + 1, ","));
+		robot.v.y = atoi(strtok(NULL, ","));
+
+		Robot *r = malloc(sizeof(Robot));
+		*r = robot;
+
+		LIST_INSERT_HEAD(&game->robots, r, link);
+
+	}
+
+	Robot *rob, *temp;
+	LIST_FOREACH(rob, &game->robots, link) {
+		printf("Robot p=(%d,%d) v=(%d,%d)\n",
+			   rob->p.x, rob->p.y, rob->v.x, rob->v.y);
+	}
+
+	LL_FOREACH_SAFE(list, it, tmp)
+	{
+		free(it->str);
+		free(it);
+	}
+
 }
